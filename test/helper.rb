e5e117d7097c27225071d2415e7a592cc85c78ae
Jimmi Dyson <jimmidyson@gmail.com>
2016-01-15 21:23:05 +0000

    Fix deprecation warning for simplecov

--- test/helper.rb
+++ test/helper.rb
@@ -18,7 +18,7 @@
 #
 require 'codeclimate-test-reporter'
 SimpleCov.start do
-  formatter SimpleCov::Formatter::MultiFormatter[
+  formatter SimpleCov::Formatter::MultiFormatter.new [
     SimpleCov::Formatter::HTMLFormatter,
     CodeClimate::TestReporter::Formatter
   ]
