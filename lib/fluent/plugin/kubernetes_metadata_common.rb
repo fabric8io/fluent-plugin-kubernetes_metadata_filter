42155228f0aa8ea15749508bed52d6428ce3daf7
Rami Ayoub <eniayoram@eniram.fi>
2020-01-08 15:22:00 +0200

    Add pod IP to kubernetes metadata

--- lib/fluent/plugin/kubernetes_metadata_common.rb
+++ lib/fluent/plugin/kubernetes_metadata_common.rb
@@ -86,7 +86,8 @@ module KubernetesMetadata
           'pod_id'         => pod_object['metadata']['uid'],
           'pod_name'       => pod_object['metadata']['name'],
           'containers'     => syms_to_strs(container_meta),
-          'host'           => pod_object['spec']['nodeName']
+          'host'           => pod_object['spec']['nodeName'],
+          'pod_ip'         => pod_object['status']['podIP']
       }
       kubernetes_metadata['annotations'] = annotations unless annotations.empty?
       kubernetes_metadata['labels'] = labels unless labels.empty?
